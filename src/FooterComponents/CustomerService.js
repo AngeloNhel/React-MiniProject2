f59c766e04a9f32d6732cac4d90fe8676930a7e7
AngeloNhel <gelogigs14@gmail.com>
2023-05-09 11:52:59 +0800

    done footer pages Our Company, Stories and News, Customer Service

--- src/FooterComponents/CustomerService.js
+++ src/FooterComponents/CustomerService.js
@@ -1,8 +1,114 @@
+import React, {useState } from "react";
+import Container from "react-bootstrap/esm/Container";
+import axios from 'axios';
+
 function CustomerService () {
+
+
+    const [email, setEmail] = useState('');
+    const [feedback, setFeedback] = useState('');
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      const newUsers =  {email, feedback};
+      try {
+         await axios.post('http://localhost:9000/feedback/JoinNow',newUsers);
+        alert('Message sent successfully');
+      } catch (error) {
+        alert('Message failed');
+      }
+    };
+
 
     return (
         <>
-         <h1> Welcome to CustomerService </h1>
+        <Container>
+            <div className="row">
+                <div className="col-12 customersServiceRow">
+                        <div className="customersServiceH2">Customer Service</div>
+                        <div className="customersServiceP">How can we help you?</div>
+                        <div><button className="customersServiceButton" data-bs-toggle="modal" data-bs-target="#exampleModal">Send a message</button></div>
+                </div>
+                
+                <div className="col-12 customersServiceCol">
+                    <div>Share your thoughts! We welcome all questions, comments and feedback – it helps us make your experience at Café la crème the best it can be. Please select the category below that best fits your comment or question, then e-mail us. We’ll respond as quickly as possible.</div>
+                </div>
+
+                <div className="col-12 customersServiceCol">
+                    <div className="customerServiceBold">Our products and stores</div>
+                    <div>Please e-mail <span className="customerServiceBold">customer@rustancoffee.ph</span> if you have something to ask or say about your experiences with our beverages, merchandise or retail stores.</div>
+                </div>
+
+                <div className="col-12 customersServiceCol">
+                    <div className="customerServiceBold">Our company</div>
+                    <div>If you would like to find out more about Café la crème as a company, including our corporate social responsibility efforts or investor relations, please e-mail <span className="customerServiceBold">marketing@rustancoffee.ph</span>.</div>
+                </div>
+
+                <div className="col-12 customersServiceCol">
+                    <div className="customerServiceBold">Our website</div>
+                    <div>If you have a technical issue to report or question regarding this web site, then by all means e-mail<span className="customerServiceBold"> marketing@rustancoffee.ph.</span>.</div>
+                </div>
+
+                <div className="col-12 customersServiceCol">
+                    <div className="customerServiceBold">Our careers</div>
+                    <div>If you would like to be part of the Café la crème Philippines team<span className="customerServiceBold"> click on Careers.</span>.</div>
+                </div>
+
+                <div className="col-12 customersServiceCol">
+                    <div className="customerServiceBold">Café la crème Rewards Customer Care</div>
+                    <div>If you would like to contact Café la crème by postal mail, phone or e-mail, please direct your questions and comments to:</div>
+                </div>
+
+                <div className="col-12 customersServiceCol">
+                    <div className="customerServiceBold">Café la crème Customer Relations</div>
+                    <div>Rustan Coffee Building, 14 Jupiter St. cor. Asteroid St.,
+                    <br/> Bel-Air 1, Makati City, Philippines 1209
+                    <br/>Tel: +632-8462-6262 (Metro Manila), 1-800-10-4626262 (domestic toll-fee via PLDT)
+                    <br/>Monday to Friday, 9AM - 9PM
+                    <br/>Saturday, 10AM - 8PM
+                    <br/><span  className="customerServiceBold">E-mail: card@Café la crème.ph</span></div>
+                </div>
+            </div>
+
+            
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <form onSubmit={handleSubmit}>
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Send a message</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Email</span>
+                        <input 
+                        type="text" class="form-control" 
+                        aria-label="Sizing example input" 
+                        aria-describedby="inputGroup-sizing-default"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}/>
+                    </div>
+                    <div class="form-floating">
+                        <textarea 
+                        class="form-control CustomersServiceTextarea" 
+                        placeholder="Leave a comment here" 
+                        id="floatingTextarea2"
+                        value={feedback}
+                        onChange={(event) => setFeedback(event.target.value)}></textarea>
+                        <label for="floatingTextarea2">Comments</label>
+                    </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" className="customersServiceButton">Submit</button>
+                    </div>
+                </form>
+                </div>
+            </div>
+            </div>
+        
+
+        </Container>
         </>
     );
 }
--- src/FooterComponents/OurCompany.js
+++ src/FooterComponents/OurCompany.js
@@ -1,8 +1,53 @@
+import Container from "react-bootstrap/esm/Container";
+
 function OurCompany () {
 
     return (
         <>
-         <h1> Welcome to Our Company </h1>
+        <Container>
+            <h1 className="h1OurCompany">Our Company </h1>
+            
+            <div className="row align-items-top"> 
+                <div className="col-md-6">
+                    <div><img className="img-fluid" src="/Assets/Section1OurCompany.jpg" alt="Section Coffee & Craft"></img></div>
+                </div>
+                <div className="col-md-6">
+                    <div><h2>Coffee & Craft</h2></div>
+                    <div><p>It takes many hands to craft the perfect cup of coffee – from the farmers who tend to the red-ripe coffee cherries, 
+                        to the master roasters who coax the best from every bean, and to the barista who serves it with care.
+                        We are committed to the highest standards of quality and service,
+                        embracing our heritage while innovating to create new experiences to savor.</p></div>
+                </div>
+            </div>
+
+            <div className="row align-items-top"> 
+                <div className="col-md-6">
+                    <div><img className="img-fluid" src="/Assets/Section2OurCompany.jpg" alt="Section Coffee & Craft"></img></div>
+                </div>
+                <div className="col-md-6">
+                    <div><h2>Our Partners</h2></div>
+                    <div><p>We like to say that we are not in the coffee business serving people,
+                            but in the people business serving coffee. Our employees – who we call partners – are at the heart of the Starbucks experience.
+                            We are committed to making our partners proud and investing in their health,
+                            well-being and success and to creating a culture of belonging where everyone is welcome.</p></div>
+                </div>
+            </div>
+
+
+            <div className="row align-items-top"> 
+                <div className="col-md-6">
+                    <div><img className="img-fluid" src="/Assets/Section3OurCompany.jpg" alt="Section Coffee & Craft"></img></div>
+                </div>
+                <div className="col-md-6">
+                    <div><h2>We Believe in the Pursuit of Doing Good</h2></div>
+                    <div><p>As it has been from the beginning, our purpose goes far beyond profit. 
+                        We believe Starbucks can, and should, have a positive impact on the communities we serve.</p></div>
+                </div>
+            </div>
+            
+                
+             
+        </Container>
         </>
     );
 }
